9b0115629112d595a5f529ec1364516e13e00911
Yun-Nakyung <93471500+Yun-Nakyung@users.noreply.github.com>
2023-08-09 18:02:02 +0900

    create posts

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,73 @@
+<template>
+	<header>
+		<nav class="navbar navbar-expand-sm bg-body-tertiary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">YUN</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto">
+						<!--
+                RouterLink 
+                : Vue Router에서 페이지를 이동할 때 사용
+                  페이지를 리로딩 하지 않고 URL에 매핑된 페이지를 렌더링
+            -->
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/"
+								>Home</RouterLink
+							>
+						</li>
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/about"
+								>About</RouterLink
+							>
+						</li>
+						<!-- ------------------------------------------------------ -->
+						<!-- <li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="/">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/about">About</a>
+						</li> -->
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/posts"
+								>게시글</RouterLink
+							>
+						</li>
+					</ul>
+					<div class="d-flex" role="search">
+						<button
+							class="btn btn-outline-success"
+							type="button"
+							@click="goPage"
+						>
+							글쓰기
+						</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goPage = () => {
+	router.push({
+		name: 'PostCreate',
+	});
+};
+</script>
+
+<style lang="scss" scoped></style>
